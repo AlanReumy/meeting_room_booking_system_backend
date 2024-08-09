42ed57bd35ecec1658bdeaa02a2cb835e80fa567
AlanReumy <alanreumy@outlook.com>
2024-08-09 15:49:14 +0800

    feat: 抽离常量到环境变量

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
 import { createClient } from 'redis';
+import { ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({
@@ -8,16 +9,18 @@ import { createClient } from 'redis';
     RedisService,
     {
       provide: 'REDIS_CLIENT',
-      async useFactory() {
+      async useFactory(configService: ConfigService) {
         const client = createClient({
           socket: {
-            host: 'localhost',
+            host: configService.get('redis_server_host'),
           },
           database: 1,
         });
+        console.log(client);
         await client.connect();
         return client;
       },
+      inject: [ConfigService],
     },
   ],
   exports: [RedisService],
